d127c49580ceaf3f9e6c969674dc34e87557ba25
RavitejaY28041994 <ravitejayendamuri@gmail.com>
2021-06-05 14:00:43 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace CSharpAutomation
+{
+    class Program
+    {
+        [Test]
+        public static void openBrowser()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            ChromeDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://facebook.com");
+        }
+    }
+}
